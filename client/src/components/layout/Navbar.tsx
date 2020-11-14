799ea9a1f0e8b56773605ec86eca7e97623198c9
Bong Lee <bongster228@github.com>
2020-11-13 22:44:48 -0800

    added routing to navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,27 +1,37 @@
 import {
   AppBar,
-  Button,
   createStyles,
+  Grid,
   IconButton,
   makeStyles,
+  Menu,
+  MenuItem,
   Theme,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
       flexGrow: 1,
+    },
+    homeButton: {
+      "& .MuiSvgIcon-root": {
+        width: "3rem",
+        height: "3rem",
+        marginRight: theme.spacing(1),
+      },
+
+      color: "inherit",
     },
   })
 );
@@ -29,21 +39,62 @@ const useStyles = makeStyles((theme: Theme) =>
 const Navbar = () => {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Civic App
-          </Typography>
+          <Grid justify="space-between" container>
+            {/* Home button */}
+            <IconButton
+              className={classes.homeButton}
+              edge="start"
+              component={Link}
+              to="/"
+            >
+              <AccountBalanceIcon />
+              <Typography variant="h6" className={classes.title}>
+                Civic App
+              </Typography>
+            </IconButton>
+
+            {/* Right Menu */}
+            <IconButton
+              onClick={handleClick}
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem
+                component={Link}
+                to="/representatives"
+                onClick={handleClose}
+              >
+                Representatives
+              </MenuItem>
+              <MenuItem component={Link} to="/elections" onClick={handleClose}>
+                Elections
+              </MenuItem>
+            </Menu>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
